b17b367ff3def895d8f2bdd7d3a92481fca9a3e6
David Cole <david.cole@fanduel.com>
2016-02-26 19:12:57 +0000

    refactored fibonacci recursion example

--- examples/functional/recursion.js
+++ examples/functional/recursion.js
@@ -1,16 +1,23 @@
-export function fibonacciMaker(sequenceLength, sequence = [0, 1]) {
-	if(sequenceLength < 3) {
+export function makeFibonacciSequence(sequenceLength, sequence = [0, 1]) {
+	if(sequenceLength <= 2) {
 		return sequence.slice(0, sequenceLength);
 	}
 
-	let clonedSequence = sequence.slice(0);
-	const lastTwoInSequence = sequence.slice(-2);
-
-	clonedSequence.push(lastTwoInSequence[0] + lastTwoInSequence[1]);
-
-	if (clonedSequence.length < sequenceLength) {
-		return fibonacciMaker(sequenceLength, clonedSequence);
+	if (sequence.length < sequenceLength - 1) {
+		return makeFibonacciSequence(sequenceLength, iterateFibonacciSequence(sequence));
 	} else {
-		return clonedSequence;
+		return iterateFibonacciSequence(sequence);
 	}
+}
+
+function iterateFibonacciSequence(sequence) {
+	return safePush(sequence, sumLastTwo(sequence));
+}
+
+function sumLastTwo(sequence) {
+	return sequence.slice(-2).reduce((a, b) => a + b);
+}
+
+function safePush(array, value) {
+	return array.slice(0).concat([value]);
 }
--- examples/functional/recursion.test.js
+++ examples/functional/recursion.test.js
@@ -1,5 +1,5 @@
 import chai from 'chai';
-import {fibonacciMaker} from './recursion';
+import {makeFibonacciSequence} from './recursion';
 
 const expect = chai.expect;
 
@@ -7,19 +7,19 @@ describe('functional', () => {
 	describe('recursion', () => {
 		describe('fibonacci maker', () => {
 			it('creates a fibonacci sequence of 1 number', () => {
-				expect(fibonacciMaker(1)).to.deep.equal([0]);
+				expect(makeFibonacciSequence(1)).to.deep.equal([0]);
 			});
 
 			it('creates a fibonacci sequence of 2 numbers', () => {
-				expect(fibonacciMaker(2)).to.deep.equal([0, 1]);
+				expect(makeFibonacciSequence(2)).to.deep.equal([0, 1]);
 			});
 
 			it('creates a fibonacci sequence of 3 numbers', () => {
-				expect(fibonacciMaker(3)).to.deep.equal([0, 1, 1]);
+				expect(makeFibonacciSequence(3)).to.deep.equal([0, 1, 1]);
 			});
 
 			it('creates a fibonacci sequence of 10 numbers', () => {
-				expect(fibonacciMaker(10)).to.deep.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+				expect(makeFibonacciSequence(10)).to.deep.equal([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
 			});
 		});
 	});
