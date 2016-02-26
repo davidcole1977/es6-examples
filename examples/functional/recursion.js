8156cb3504cb7077ba6db1b9a993cc5960302f05
David Cole <david.cole@fanduel.com>
2016-02-26 19:19:15 +0000

    refactor(recursion): remove superfluous helper function from fibonacci example

--- examples/functional/recursion.js
+++ examples/functional/recursion.js
@@ -3,21 +3,15 @@ export function makeFibonacciSequence(sequenceLength, sequence = [0, 1]) {
 		return sequence.slice(0, sequenceLength);
 	}
 
-	if (sequence.length < sequenceLength - 1) {
-		return makeFibonacciSequence(sequenceLength, iterateFibonacciSequence(sequence));
-	} else {
-		return iterateFibonacciSequence(sequence);
-	}
-}
+	const interatedSequence = sequence.concat(sumLastTwo(sequence));
 
-function iterateFibonacciSequence(sequence) {
-	return safePush(sequence, sumLastTwo(sequence));
+	if (sequence.length < sequenceLength - 1) {
+		return makeFibonacciSequence(sequenceLength, interatedSequence);
+	} else {
+		return interatedSequence;
+	}
 }
 
 function sumLastTwo(sequence) {
 	return sequence.slice(-2).reduce((a, b) => a + b);
-}
-
-function safePush(array, value) {
-	return array.slice(0).concat([value]);
 }
