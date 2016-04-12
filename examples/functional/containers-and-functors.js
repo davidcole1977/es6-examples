d075d064dc06c26f11cb012fe845fd14f85b53d4
David Cole <hallo@davidcole.info>
2016-04-12 21:52:52 +0100

    add basic container and maybe functor examples

--- examples/functional/containers-and-functors.js
+++ examples/functional/containers-and-functors.js
@@ -1,3 +1,31 @@
-export function foo() {
-	return 'bar';
+// Container
+
+export class Container {
+  constructor(value) {
+    this.__value = value;
+  }
+
+  map(func) {
+    return new Container(func(this.__value));
+  }
 }
+
+Container.of = (value) => new Container(value);
+
+// Maybe
+
+export class Maybe {
+  constructor(value) {
+    this.__value = value;
+  }
+
+  isNothing() {
+    return (this.__value === null || this.__value === undefined);
+  }
+
+  map(func) {
+    return this.isNothing() ? Maybe.of(null) : Maybe.of(func(this.__value));
+  }
+}
+
+Maybe.of = (value) => new Maybe(value);
--- examples/functional/containers-and-functors.test.js
+++ examples/functional/containers-and-functors.test.js
@@ -1,12 +1,45 @@
 import chai from 'chai';
-import {foo} from './containers-and-functors';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import {Container, Maybe} from './containers-and-functors';
 
 const expect = chai.expect;
+chai.use(sinonChai);
 
 describe('functional', () => {
-	describe('containerising', () => {
-		it('stuff', () => {
-			expect(foo()).to.equal('bar');
-		});
+	describe.only('containers and functors', () => {
+    let mapSpy;
+    const double = (value) => value * 2;
+
+    beforeEach(() => {
+      mapSpy = sinon.spy();
+    });
+
+		describe('Container', () => {
+      it('maps a simple function', () => {
+        const ten = Container.of(10);
+        ten.map(double).map(mapSpy);
+        expect(mapSpy).to.have.been.calledWith(20);
+      });
+    });
+
+    describe('Maybe', () => {
+      describe('instantiated with a valid value', () => {
+        it('maps a simple function', () => {
+          const ten = Maybe.of(10);
+          ten.map(double).map(mapSpy);
+          expect(mapSpy).to.have.been.calledWith(20);
+        });
+      });
+
+      describe('instantiated with a null value', () => {
+        it('does not map the supplied function', () => {
+          const nothing = Maybe.of(null);
+          nothing.map(double).map(mapSpy);
+          expect(mapSpy).to.not.have.been.called;
+        });
+      });
+    });
 	});
 });
